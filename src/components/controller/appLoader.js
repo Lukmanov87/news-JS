0c4b45a199467ea62ba8cff7f8dab660e8dfa8a1
Lukmanov87 <61542812+Lukmanov87@users.noreply.github.com>
2022-06-30 19:41:00 +0300

    init

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'e6603b216d0b4aed8aafcb506121cde3'
         });
     }
 }
