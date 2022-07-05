cff930468107a0faadebc05e7cb2d0d0beb03119
Lukmanov87 <61542812+Lukmanov87@users.noreply.github.com>
2022-07-05 19:17:36 +0300

    feat: add interfaceLoader.ts & migrate loader.js

--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -1,19 +1,18 @@
-class Loader {
-    constructor(baseLink, options) {
-        this.baseLink = baseLink;
-        this.options = options;
-    }
+import IResponseConfig from '../../interfaces/interfaceLoader';
 
-    getResp(
-        { endpoint, options = {} },
-        callback = () => {
+class Loader {
+    constructor(private baseLink: string, private options: { apiKey: string }) {}
+
+    getResp<T>(
+        responseConfig: IResponseConfig,
+        callback: (data: T) => void = () => {
             console.error('No callback for GET response');
         }
     ) {
-        this.load('GET', endpoint, callback, options);
+        this.load('GET', callback, responseConfig);
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response) {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,8 +22,8 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
+    makeUrl({ endpoint, options = {} }: IResponseConfig) {
+        const urlOptions: { [url: string]: string } = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
         Object.keys(urlOptions).forEach((key) => {
@@ -34,8 +33,8 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
-        fetch(this.makeUrl(options, endpoint), { method })
+    load<T>(method: string, callback: (data: T) => void, responseConfig: IResponseConfig) {
+        fetch(this.makeUrl(responseConfig), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
             .then((data) => callback(data))
